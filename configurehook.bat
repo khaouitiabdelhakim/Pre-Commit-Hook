2e9e607b0a2c0edf3984f7bfac35f44312dfa006
ChoukriMehdi <me.choukri1234@gmail.com>
2024-07-18 01:15:47 +0200

    precommit

---
+++ configurehook.bat
@@ -0,0 +1,16 @@
+[core]
+	repositoryformatversion = 0
+	filemode = false
+	bare = false
+	logallrefupdates = true
+	symlinks = false
+	ignorecase = true
+	hooksPath = git-hook
+[submodule]
+	active = .
+[remote "origin"]
+	url = https://github.com/khaouitiabdelhakim/Pre-Commit-Hook.git
+	fetch = +refs/heads/*:refs/remotes/origin/*
+[branch "main"]
+	remote = origin
+	merge = refs/heads/main
